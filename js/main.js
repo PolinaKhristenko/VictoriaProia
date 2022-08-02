cd67fa97f3e301862fc2b0b2ec3dff1f8a587bf5
PolinaTimofeeva <polleyw@mail.ru>
2022-08-03 01:40:11 +0300

    drafts

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-let excursions = [
-    
-]
+$(document).ready(function () {
+    $('.reviews__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 20000,
+    });
+  });     
