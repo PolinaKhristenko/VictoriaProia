c572ff95b6286adabd5589aa765745a6263291ed
PolinaTimofeeva <polleyw@mail.ru>
2022-08-05 00:43:10 +0300

    slider decorations

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('.reviews__slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 20000,
+        //autoplay: true,
+        //autoplaySpeed: 20000,
     });
   });     
