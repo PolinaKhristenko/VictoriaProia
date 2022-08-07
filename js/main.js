601e2c5f5485c331808e720f00b5da1a92109fdd
PolinaTimofeeva <polleyw@mail.ru>
2022-08-07 15:24:00 +0300

    cards

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-$(document).ready(function () {
-    $('.reviews__slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        //autoplay: true,
-        //autoplaySpeed: 20000,
-    });
-  });     
+// Cards info 
+
+
+tours.forEach(tour => {
+	var card = document.querySelector('.cards')
+  
+	card.innerHTML += `
+          <a class="cards__block" id="${tour.id}">
+            <img class="cards__img" src="${tour.img}" alt="Фото с экскурсии" />
+            <h3 class="cards__title">${tour.title}</h3>
+            <p class="cards__place">${tour.place}</p>
+            <p class="cards__time">${tour.time}</p>
+            <p class="cards__price">от ${tour.price}€ с группы за 1 час</p> 
+          </a>
+  `
+})
---
+++ js/slider.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.reviews__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+  });     
