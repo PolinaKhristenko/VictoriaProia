15e1bed50c6e43904fb4d4e8a0d28e1b06b48e01
PolinaTimofeeva <polleyw@mail.ru>
2022-08-10 01:20:47 +0300

    adaptive tricks

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,30 @@
 $(document).ready(function () {
     $('.reviews__slider').slick({
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                arrows: false,
+                dots: true
+              }
+            },
+        ]
     });
 
     $('.about__slider-body').slick({
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                arrows: false,
+                dots: true
+              }
+            },
+        ]
     });
   });  
   
